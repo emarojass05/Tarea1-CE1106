bb4430e9e7526dd8fd6b0da7fbcd2b5da6e730f9
unknown <gabrielfernandezvargas2@gmail.com>
2024-08-23 22:10:16 -0600

    Commit interface

--- BaseCode.asm
+++ BaseCode.asm
@@ -7,20 +7,28 @@ LF EQU 10
 .MODEL SMALL
 .STACK 200 ; Se define la pila
 .DATA ; Se definen datos
-       mensaje1 DB cr,lf,'Programa que calcula el area de un trapecio.$'
+       mensaje1 DB cr,lf,'Calculadora de Figuras$'
+       mensajeF DB cr,lf,'Que figura te gustaria seleccionar?$'
+       mensajeT DB cr,lf,'Este es el trapecio, presione 1 para pasar a los calculos$'
+       mensajeC DB cr,lf,'Presione 1 para Area y 2 para perimetro?$'
        mensaje2 DB cr,lf,'Ingresa la base menor: (presiona enter)$'
        mensaje3 DB cr,lf,'Ingresa la base mayor: (presiona enter)$'
        mensaje4 DB cr,lf,'Presiona cualquier tecla para continuar$'
-       mensaje5 DB cr,lf,'Presiona 1 para calcular el area del trapecio o  presiona 0 para salir.$'
+       mensaje5 DB cr,lf,'Presiona 1 si estas activo y 0 para salir.$'
        mensaje6 DB cr,lf,'Ingresa la altura: (presiona enter)$'
        mensaje10 DB cr,lf,'El area del trapecio es:$'
-       salir DB cr,lf,'Saliendo del programa presione cualquier tecla..$'
+       salir DB cr,lf,'Saliendo del programa presione cualquier tecla..$' 
+       
+       ;Mensajes de Triangulo
+       mensaje12 DB cr,lf,'El area del triangulo es:$'
+       mensajeCt DB cr,lf,'Presione 1 para Area y 2 para perimetro?$'
+       mensajeTri DB cr,lf,'Este es el triangulo, presione 1 para pasar a los calculos$'
 
        f1 DW ?
        f2 DW ?
        f3 DW ?
 
-       resultado DB cr,lf,'el area del trapecio es : $'
+       resultado DB cr,lf,'El area del trapecio es: $'
        espa DB ' ',cr,lf,'$'
 
        c  equ 02
@@ -35,10 +43,7 @@ inicio:
     MOV ax,Data
     MOV DS,AX
 
-    MOV AH,00H
-    MOV AL,03H
-    INT 10H
-
+    ; Mostrar el mensaje inicial
     MOV AH,09H
     LEA DX,mensaje1
     INT 21H
@@ -46,6 +51,7 @@ inicio:
     LEA DX,espa
     INT 21H
 
+    ; Mostrar el mensaje de confirmación
     MOV AH,09H
     LEA DX,mensaje5
     INT 21H
@@ -53,14 +59,80 @@ inicio:
     LEA DX,espa
     INT 21H
 
+    ; Leer la entrada del usuario
     MOV AH,01H
     INT 21H
+    SUB AL,30H        ; Convertir de ASCII a número
 
-    SUB AL,30H
+    ; Comparar la entrada
     CMP AL,00H
-    JE fin
+    JE fin            ; Si la entrada es 0, ir al final (salir del programa)
     CMP AL,01H
-    JE multiplica
+    JE seleccion_figura
+
+    ; Si la entrada no es 0 ni 1, volver al inicio
+    JMP inicio
+
+seleccion_figura:
+    ; Mostrar el mensaje de selección de figura
+    MOV AH,09H
+    LEA DX,mensajeF
+    INT 21H
+
+    LEA DX,espa
+    INT 21H
+
+    ; Leer la entrada del usuario para la figura
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H        ; Convertir de ASCII a número
+
+    ; Comparar la entrada para la figura
+    CMP AL,01H
+    JE trapecio     ; Si la entrada es 1, ir a la rutina de cálculo
+    
+    CMP AL, 02H
+    JE triangulo
+
+    ; Si la entrada no es 1, volver al inicio
+    JMP inicio
+
+;Trapecio funciones    
+trapecio:
+    MOV AH, 09H
+    LEA DX, mensajeT    
+    INT 21H
+    
+    LEA DX, espa
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H
+    
+    CMP AL, 01H
+    JE seleccion_calculoT
+    
+    JMP inicio
+    
+seleccion_calculoT:
+    MOV AH, 09H
+    LEA DX,mensajeC
+    INT 21H
+    
+    LEA DX, espa
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H 
+    
+    CMP AL,01H
+    JE multiplica 
+    
+    JMP inicio
+    
+    
 
 multiplica:
     MOV AH,00H
@@ -98,14 +170,14 @@ multiplica:
     LEA DX,resultado
     INT 21H
 
-    MOV AX,f1 ; base menor
-    ADD AX,f2 ; base menor + base mayor
-    MOV BX,c  ; 2
-    DIV BL    ; (base menor + base mayor) / 2
-    MOV BX,f3 ; altura
-    MUL BX    ; ((base menor + base mayor) / 2) * altura
+    MOV AX,f1        ; base menor
+    ADD AX,f2        ; base menor + base mayor
+    MOV BX,c         ; 2
+    DIV BL           ; (base menor + base mayor) / 2
+    MOV BX,f3        ; altura
+    MUL BX           ; ((base menor + base mayor) / 2) * altura
 
-    MOV CX,AX ; Guardar el resultado en CX
+    MOV CX,AX        ; Guardar el resultado en CX
     CALL PRINT_NUM_UNS ; Imprimir el número
 
     LEA DX,espa
@@ -114,16 +186,110 @@ multiplica:
     LEA DX,mensaje4
     INT 21H
 
-    MOV AH,01h
+    MOV AH,01H
     INT 21H
+    JMP inicio       ; Regresar al inicio
+    
+;Triangulo funciones
+
+triangulo:
+    MOV AH, 09H
+    LEA DX, mensajeTri    
+    INT 21H
+    
+    LEA DX, espa
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H
+    
+    CMP AL, 01H
+    JE seleccion_calculoTri
+    
     JMP inicio
+    
+seleccion_calculoTri:
+    MOV AH, 09H
+    LEA DX,mensajeCt
+    INT 21H
+    
+    LEA DX, espa
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    SUB AL,30H 
+    
+    CMP AL,01H
+    JE multiplica_tri 
+    
+    JMP inicio
+         
+multiplica_tri:
+    MOV AH,00H
+    MOV AL,03H
+    INT 10H
+
+    MOV AH,09H
+    LEA DX,mensaje12
+    INT 21H  
+
+    LEA DX,espa
+    INT 21H
+
+    LEA DX,mensaje2
+    INT 21H
+
+    CALL SCAN_NUM
+    MOV f1,CX 
+
+    MOV AH,09H
+    LEA DX,mensaje3
+    INT 21H 
+
+    CALL SCAN_NUM
+    MOV f2,CX
+
+    MOV AH,09H
+    LEA DX,mensaje6
+    INT 21H 
+
+    CALL SCAN_NUM
+    MOV f3,CX
+
+    MOV AH,09H
+    LEA DX,resultado
+    INT 21H
+
+    MOV AX,f1        ; base menor
+    ADD AX,f2        ; base menor + base mayor
+    MOV BX,c         ; 2
+    DIV BL           ; (base menor + base mayor) / 2
+    MOV BX,f3        ; altura
+    MUL BX           ; ((base menor + base mayor) / 2) * altura
+
+    MOV CX,AX        ; Guardar el resultado en CX
+    CALL PRINT_NUM_UNS ; Imprimir el número
+
+    LEA DX,espa
+    INT 21H
+
+    LEA DX,mensaje4
+    INT 21H
+
+    MOV AH,01H
+    INT 21H
+    JMP inicio       ; Regresar al inicio
+    
+    
 
 fin:
     MOV AH,09H
     LEA DX,salir
     INT 21H 
 
-    MOV AH,01h
+    MOV AH,01H
     INT 21H
 
     MOV AH,01H
